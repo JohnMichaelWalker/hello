5a24b9be1659ec4966aca65102533de89a762b89
John Walker <john_walker_333@hotmail.com>
2018-11-22 22:14:21 +0000

    Updates

---
+++ precache-manifest.63e50760e4e5e1d2efd8ea4e205bc2e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c9c085a1776314c1812c",
+    "url": "/hello/static/js/runtime~main.c9c085a1.js"
+  },
+  {
+    "revision": "839d92e9d1fc8015aa54",
+    "url": "/hello/static/js/main.839d92e9.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/hello/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "839d92e9d1fc8015aa54",
+    "url": "/hello/static/css/main.197eb076.chunk.css"
+  },
+  {
+    "revision": "867fe51663b0ee6acd9503ef98b97185",
+    "url": "/hello/index.html"
+  }
+];
